c271b1b53d9de70fb47792d906a188035e1b372b
Abdelrahman <ayahia1@swarthmore.edu>
2022-08-28 12:53:43 -0400

    updates

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost: "udagram-api-dev2222222.us-east-1.elasticbeanstalk.com",
 };
 
 /*
